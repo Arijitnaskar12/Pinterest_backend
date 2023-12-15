90be32ecc8395600a18663c22e217228bfe3353d
ArijitNaskar12 <arijitnaskar378@gmail.com>
2023-12-15 11:10:38 +0530

    Pinterest

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
+const { configDotenv } = require('dotenv');
 const mongoose = require('mongoose');
 const plm=require('passport-local-mongoose');
-mongoose.connect('mongodb+srv://Arijitnaskar12:Arijit*1@cluster0.sugpcsj.mongodb.net/pininterest')
+require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI);
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   fullname: { type: String, required: true },
